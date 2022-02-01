cb6adf6ebe6c878f71bd9c14f605c033024d0524
Anton Schirg <anton.schirg@posteo.de>
2022-02-01 15:52:53 +0100

    loop pattern

--- nidaq_pattern.py
+++ nidaq_pattern.py
@@ -26,7 +26,7 @@ def frequency_MHz_to_bin(x, y):
     return (((1 << 15 | x_int) << 16) | (1 << 15 | y_int)).astype(np.uint32)
 
 
-def project_patterns(patterns_degree, rate, reset_when_done = True):
+def project_patterns(patterns_degree, rate, reset_when_done = True, loop = False):
     # patterns_degree dimensions: [pattern, sample in pattern, axis (x,y)]
     if len(patterns_degree.shape) == 2:
         patterns_degree = patterns_degree[np.newaxis, :, :]
@@ -44,16 +44,19 @@ def project_patterns(patterns_degree, rate, reset_when_done = True):
 
     sys = nidaqmx.system.System.local()
     dev = sys.devices['Dev1']
-    for pattern_nr in range(num_patterns):
-        try:
-            with nidaqmx.Task() as task:
-                task.do_channels.add_do_chan('Dev1/port0:Dev1/port3', line_grouping=nidaqmx.constants.LineGrouping.CHAN_FOR_ALL_LINES)
-                task.timing.cfg_samp_clk_timing(rate=rate, sample_mode=nidaqmx.constants.AcquisitionType.FINITE, samps_per_chan=num_samples)
-                task.export_signals.export_signal(nidaqmx.constants.Signal.START_TRIGGER, '/Dev1/PFI4')
-                task.write(scan[pattern_nr], auto_start=True)
-                task.wait_until_done()
-        except KeyboardInterrupt:
-            break
+    try:
+        first = True
+        while first or loop:
+            for pattern_nr in range(num_patterns):
+                with nidaqmx.Task() as task:
+                    task.do_channels.add_do_chan('Dev1/port0:Dev1/port3', line_grouping=nidaqmx.constants.LineGrouping.CHAN_FOR_ALL_LINES)
+                    task.timing.cfg_samp_clk_timing(rate=rate, sample_mode=nidaqmx.constants.AcquisitionType.FINITE, samps_per_chan=num_samples)
+                    task.export_signals.export_signal(nidaqmx.constants.Signal.START_TRIGGER, '/Dev1/PFI4')
+                    task.write(scan[pattern_nr], auto_start=True)
+                    task.wait_until_done()
+            first = False
+    except KeyboardInterrupt:
+        pass
 
     if reset_when_done:
         dev.reset_device()
@@ -67,8 +70,9 @@ def reset_daq():
 
 def main():
     l = np.linspace(0, 2*np.pi, 500)
-    pattern = np.stack([np.sin(l) * 0.5, np.cos(l) * 0.5], axis=1)
-    project_patterns(pattern, rate=20000.0)
+    #pattern = np.stack([np.sin(l) * 0.5, np.cos(l) * 0.5], axis=1)
+    pattern = np.zeros((1000, 2))
+    project_patterns(pattern, rate=1000.0, loop = True)
 
 
 if __name__ == "__main__":
