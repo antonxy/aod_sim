1ae4b966da4f57b0fa19f7d305d932e881f66273
Anton Schirg <anton.schirg@posteo.de>
2022-02-02 16:12:47 +0100

    record using ring buffer

--- sim_hardware_system.py
+++ sim_hardware_system.py
@@ -12,12 +12,14 @@ class SIMHardwareSystem:
         self.camera.configuration = {
             'roi': (1, 1, 1008, 1008),
             'trigger': 'external exposure start & software trigger',
-            #'trigger': 'auto sequence',
             'acquire': 'auto',
         }
         self.configure_camera(1e-3)
 
+        self.camera.record(number_of_images=7, mode='ring buffer')
+
     def disconnect(self):
+        self.camera.stop()
         self.camera.close()
         self.camera = None
 
@@ -32,14 +34,7 @@ class SIMHardwareSystem:
         exposure_time_sec = patterns_deg.shape[1] / pattern_rate_Hz
         self.configure_camera(exposure_time_sec)
 
-        self.camera.record(number_of_images=7, mode='sequence non blocking')
-
         nidaq_pattern.project_patterns(patterns_deg, pattern_rate_Hz)
-        while True:
-            running = self.camera.rec.get_status()['is running']
-            if not running:
-                break
-            time.sleep(0.001)
 
         images, metadatas = self.camera.images()
         return np.stack(images)
