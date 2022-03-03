2c7e60531003f8d39fee848eaaa6dad558b1a14f
Anton Schirg <anton.schirg@posteo.de>
2022-03-03 18:45:43 +0100

    measure gratings update

--- measure_line_gratings.py
+++ measure_line_gratings.py
@@ -21,12 +21,15 @@ class MeasureLineGratingsDialog(QtWidgets.QDialog):
         form_layout = QtWidgets.QFormLayout()
 
         self.grating_dot_distance_1_txt = QtWidgets.QLineEdit(str(params['grating_dot_distances'][0]))
+        self.grating_dot_distance_1_txt.returnPressed.connect(self.take_image)
         form_layout.addRow("Grating dot distance 1 [deg]", self.grating_dot_distance_1_txt)
 
         self.grating_dot_distance_2_txt = QtWidgets.QLineEdit(str(params['grating_dot_distances'][1]))
+        self.grating_dot_distance_2_txt.returnPressed.connect(self.take_image)
         form_layout.addRow("Grating dot distance 2 [deg]", self.grating_dot_distance_2_txt)
 
         self.grating_dot_distance_3_txt = QtWidgets.QLineEdit(str(params['grating_dot_distances'][2]))
+        self.grating_dot_distance_3_txt.returnPressed.connect(self.take_image)
         form_layout.addRow("Grating dot distance 3 [deg]", self.grating_dot_distance_3_txt)
         self.grating_dot_distance_txts = [
             self.grating_dot_distance_1_txt,
@@ -66,7 +69,7 @@ class MeasureLineGratingsDialog(QtWidgets.QDialog):
         orientation_deg = self.global_params['orientation_deg']
         distance_between_gratings = self.global_params["distance_between_gratings"]
 
-        pattern_deg = lmi_pattern.line_lmi_pattern_deg(2, 1, grating_dot_distances, distance_between_gratings, orientation_rad=np.deg2rad(orientation_deg))
+        pattern_deg = lmi_pattern.line_lmi_pattern_deg(2, 1, [x * 2 for x in grating_dot_distances], distance_between_gratings, orientation_rad=np.deg2rad(orientation_deg))
 
         pattern_deg = np.reshape(pattern_deg, (1, -1, 2))  # All steps in single frame
 
@@ -74,7 +77,7 @@ class MeasureLineGratingsDialog(QtWidgets.QDialog):
 
     def take_image(self):
         pattern_deg = self.grating_pattern()
-        pattern_deg = np.repeat(pattern_deg, 100, axis=1)
+        pattern_deg = np.repeat(pattern_deg, 30, axis=1)
 
         frames = self.sim_system.project_patterns_and_take_images(pattern_deg, 10000, delay_sec = 0.0)
         self.adj_frame = frames[0]
